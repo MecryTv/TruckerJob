a0f114f9f78656db9c1f42026d19ea92431817e3
MecryTv <mail.MecryTv@pm.me>
2025-04-20 16:09:16 +0100

    commit

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,13 +6,13 @@ description 'Trucker Job with custom UI'
 version '1.0.0'
 
 client_scripts {
-    './client.lua',
-    './config.lua',
+    'client.lua',
+    'config.lua',
 }
 
 ui_page 'ui/dist/ui/browser/index.html'
 
 files {
-    'ui/dist/browser/index.html',
-    'ui/dist/browser/**/*',
+    'ui/dist/ui/browser/index.html',
+    'ui/dist/ui/browser/**/*',
 }
